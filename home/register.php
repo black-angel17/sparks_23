841930e5d5e4062db0ae69dca93fbb2641104ce8
aravindakrs2 <aravindakrs2@gmail.com>
2023-09-30 13:48:30 +0530

    test 3

--- home/register.php
+++ home/register.php
@@ -8,6 +8,7 @@ Database::make_conn();
 
 Database::insert_data($_POST['team_name'],$_POST['team_id'],$_POST['member1'],$_POST['member2'],$_POST['member3'],$_POST['Department'],$_POST['Year'],$_POST['phone']);
 
+header("Location: index.html");
 
 ?>
 </pre>
